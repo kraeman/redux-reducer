909beaa820fe0e5e8d1fc02e81688da8e348a114
Krae Wind <kraewind@gmail.com>
2021-07-15 12:45:42 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,14 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {friends: []}, action){
+    let newState = state
+    if(action.type === "ADD_FRIEND"){
+        newState.friends.push(action.friend)
+        return newState
+    }else if(action.type === "REMOVE_FRIEND") {
+        let idx = newState.friends.findIndex((frien) => frien.id === action.id)
+        newState.friends.splice(idx, 1)
+        console.log(idx)
+        return newState
+    }else{
+        return newState
+    }
 }
